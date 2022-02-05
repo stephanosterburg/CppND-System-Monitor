4843cc6f29f501faf2dba370eefd6e3a7afc9679
stephanosterburg <stephanosterburg@me.com>
2022-02-05 21:08:32 +0000

    reformat code

--- src/format.cpp
+++ src/format.cpp
@@ -6,11 +6,12 @@
 using std::string;
 using std::to_string;
 
-// TODO: Complete this helper function
+// helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds) {
+string Format::ElapsedTime(long seconds)
+{
     int hrs, min, sec;
     std::stringstream h, m, s;
     string time;
